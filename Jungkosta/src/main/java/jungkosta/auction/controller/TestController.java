bff3b4dbdb3cab80b55abb32dcc9511ba1783fe7
phw11 <hyunwoo4847@gmail.com>
2016-11-15 20:44:46 +0900

    현우

---
+++ Jungkosta/src/main/java/jungkosta/auction/controller/TestController.java
@@ -0,0 +1,27 @@
+package jungkosta.auction.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+
+@Controller
+@RequestMapping("/Jungkosta/auction/*")
+public class TestController {
+   
+   private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+   
+   /**
+    * Simply selects the home view to render by returning its name.
+    */
+   @RequestMapping("/index")
+   public String home() {
+     
+      return "index";
+   }
+   
+}
