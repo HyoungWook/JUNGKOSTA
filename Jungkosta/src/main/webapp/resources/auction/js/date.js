25d729e0d475f57a9a1b163499c59d8aa4c225d9
sec <sec@samsung>
2016-11-16 11:10:49 +0900

    변경

---
+++ Jungkosta/src/main/webapp/resources/auction/js/date.js
@@ -0,0 +1,23 @@
+$(function() {
+
+	$("#buy_date").datepicker({
+		numberOfMonths : 3,
+		showButtonPanel : true,
+		changeMonth : true,
+		changeYear : true,
+		dateFormat : 'yy-mm-dd'
+	});
+	
+
+	$("#to").datepicker({
+		numberOfMonths : 3,
+		showButtonPanel : true,
+		changeMonth : true,
+		changeYear : true,
+		dateFormat : 'yy-mm-dd'
+	});
+	
+
+
+
+});
