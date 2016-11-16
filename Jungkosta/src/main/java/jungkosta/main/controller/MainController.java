f3528608e0852b630069d856e232bb3fd5ac786d
HyoungWook <rlaguddnr92@naver.com>
2016-11-16 17:21:27 +0900

    2016.11.16 17:20 형욱 메인회원가입 폼 부트스트랩, js작업

--- Jungkosta/src/main/java/jungkosta/main/controller/MainController.java
+++ Jungkosta/src/main/java/jungkosta/main/controller/MainController.java
@@ -15,9 +15,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController {
+public class MainController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -36,8 +36,13 @@ public class HomeController {
 		return "home";
 	}
 	
-	@RequestMapping("/test")
+	@RequestMapping(value="/mainSignupForm", method=RequestMethod.GET)
 	public void go(){
+		
+	}
+	
+	@RequestMapping(value="/mainSignupForm2", method=RequestMethod.GET)
+	public void mainSignupForm2(){
 		
 	}
 	
