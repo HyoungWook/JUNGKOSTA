2d0727bf08f5862bf3def95f14bf150c2b1335cb
phw11 <hyunwoo4847@gmail.com>
2016-11-17 13:39:22 +0900

    현우 auctionVo 객체 수정

--- Jungkosta/src/main/java/jungkosta/auction/controller/AuctionRegisterController.java
+++ Jungkosta/src/main/java/jungkosta/auction/controller/AuctionRegisterController.java
@@ -27,33 +27,34 @@ public class AuctionRegisterController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 
-	
 	@RequestMapping(value = "/auctionRegisterForm", method = RequestMethod.GET)
 	public void auctionRegisterForm() {
 
 	}
 
-	//auctionRegister => auctionRegisterForm 변경
+	// auctionRegister => auctionRegisterForm 변경
 	@RequestMapping(value = "/auctionRegisterForm", method = RequestMethod.POST)
 	public String auctionRegister(AuctionVO auction, HttpServletRequest request) throws Exception {
-		
-		auction.setSale_id(service.sale_id()+1);
-		auction.setEmail("aaa@aaa.com");
-	
-		//수정부분 feat : 현우
+
+		auction.setSale_id(service.sale_id() + 1);
+		auction.setEmail("qkrgusdn93@naver.com");
+
+		// 수정부분 feat : 현우
 		String buy_time = request.getParameter("buy_time_temp");
 		String auction_end_date = request.getParameter("auction_end_date_temp");
-		
+
 		buy_time += " 00:00:00.0";
 		auction_end_date += " " + request.getParameter("time_sel") + ":00.0";
-		
-		
+
 		auction.setBuy_time(Timestamp.valueOf(buy_time));
 		auction.setAuction_end_date(Timestamp.valueOf(auction_end_date));
-		//수정부분 feat : 현우
-		auction.setAutcion_id(service.auction_id()+1);
+		// 수정부분 feat : 현우
+		auction.setAutcion_id(service.auction_id() + 1);
 		
-		
+		auction.setItem_cost(auction.getAuction_stcost());
+
+		System.out.println(auction);
+
 		service.register(auction);
 		return "redirect:/home";
 	}
